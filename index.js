a07f681d403050bbdda2232864a82f379432aba7
Omkar <Omkarsubedi123.omsubedi28@gmail.com>
2025-01-11 13:28:39 -0500

    In Progress

--- index.js
+++ index.js
@@ -2,3 +2,21 @@ function toggleMenu() {
   const mobileMenu = document.querySelector(".MobileMenu");
   mobileMenu.classList.toggle("hidden");
 }
+
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      keyframes: {
+        flipY: {
+          "0%, 100%": { transform: "rotateY(0deg)" },
+          "33%": { transform: "rotateY(-90deg)" },
+          "66%": { transform: "rotateY(180deg)" },
+        },
+      },
+      animation: {
+        flipY: "flipY 3s infinite",
+      },
+    },
+  },
+};
